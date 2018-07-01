09459e9d10006417318a7989ac5acd1e2403eee7
Dang Tien Chieu <dangtien2409@gmail.com>
2018-07-01 22:59:43 +0700

    dtc

--- DA_CNPM/js/main.js
+++ DA_CNPM/js/main.js
@@ -9,5 +9,32 @@ var vehicle_manager = {
     },
     content: function () {
         $('#page-wrapper').load(this.path + "/template/page-wrapper.html");
+    },
+    confirm_in: function () {
+        var inputs=[];
+        $("#home input").each(function(){
+            inputs.push({"name":$(this).attr("name"),"value":$(this).val()});
+        });
+        if(inputs[0].value === "" || inputs[1].value=== ""){
+            alert("Không thành công. Thông tin thiếu");
+        }else{
+            alert("Thêm thành công");
+        }
+    },
+    confirm_out: function () {
+        var inputs=[];
+        $("#menu1 input").each(function(){
+            inputs.push({"name":$(this).attr("name"),"value":$(this).val()});
+        });
+        if(inputs[0].value === "" || inputs[1].value=== ""){
+            alert("Không thành công. Thông tin thiếu");
+        }else{
+            alert("Thêm thành công");
+        }
+        
+    },
+    out_load:function(){
+        $("#menu1 #dateout").val(new Date());
+        $("#menu1 #datein").val(new Date());
     }
 };
